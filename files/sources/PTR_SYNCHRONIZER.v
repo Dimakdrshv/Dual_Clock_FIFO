937bbb935438de15a87243a4e58c624156e6ef93
Dmitry Kudryashov <dimakudrashov13@gmail.com>
2026-05-26 00:07:17 +0300

    Add PTR synchronizer

---
+++ files/sources/PTR_SYNCHRONIZER.v
@@ -0,0 +1,37 @@
+`timescale 1ps / 1ps
+
+//===========================================================
+// File Path: D:/VivadoProjects/Dual_Clock_FIFO/files/sources/PTR_SYNCHRONIZER.v
+// Author: Kudryashov D.S.
+// Created On: 2026-05-25 23:43:58
+// Description: 
+//===========================================================
+
+
+module PTR_SYNCHRONIZER
+#(
+    parameter PTR_WDT = 3
+)
+(
+    // System signals
+    input wire wrclk, // w or r clk
+    input wire wrrst_n, // w or r rst_n
+    
+    // Pointers
+    input  wire [PTR_WDT - 1 : 0] wrptr, // w or r ptr
+    output reg  [PTR_WDT - 1 : 0] wrptr_sync // w or r ptr_sync
+);
+    
+    reg [PTR_WDT - 1 : 0] wrptr_reg;
+
+    always @(posedge wrclk or negedge wrrst_n) begin
+        if (!wrrst_n) begin
+            wrptr_reg  <= 'b0;
+            wrptr_sync <= 'b0;
+        end else begin
+            wrptr_reg  <= wrptr;
+            wrptr_sync <= wrptr_reg;
+        end
+    end
+    
+endmodule
