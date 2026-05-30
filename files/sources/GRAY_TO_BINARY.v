2df3d5546366a7c796110a4340b97014628c79cb
Dmitry Kudryashov <dimakudrashov13@gmail.com>
2026-05-30 23:27:11 +0300

    Update comments in B2G and G2B

--- files/sources/GRAY_TO_BINARY.v
+++ files/sources/GRAY_TO_BINARY.v
@@ -13,8 +13,8 @@ module GRAY_TO_BINARY
     parameter PTR_WDT = 3
 )
 (
-    input  wire [PTR_WDT - 1 : 0] wrptr_g,
-    output wire [PTR_WDT - 1 : 0] wrptr_b
+    input  wire [PTR_WDT - 1 : 0] wrptr_g, // write or read ptr **gray code
+    output wire [PTR_WDT - 1 : 0] wrptr_b  // write or read ptr **binary
 );
 
 
